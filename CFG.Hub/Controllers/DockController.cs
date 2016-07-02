d0687a130ecbce21b586f5e378378a1d2c30f3f4
Broomscom <brooks.kreutzer.analyst@outlook.com>
2016-07-02 16:38:24 -0600

    #SETUP - Complex this is

--- CFG.Hub/Controllers/DockController.cs
+++ CFG.Hub/Controllers/DockController.cs
@@ -9,7 +9,7 @@ namespace CFG.Hub.Controllers
     using Models;
 
     /// <summary>
-    /// Configuration hub endpoint controller
+    /// Configuration hub endpoint controller.
     /// </summary>
     /// <seealso cref="System.Web.Http.ApiController" />
     public class DockController : ApiController
@@ -20,16 +20,60 @@ namespace CFG.Hub.Controllers
         /// <returns>Pong Service Response</returns>
         [Route("Dock/Ping"), HttpGet, ConfigHubAuthorize]
         public HttpResponseMessage Ping()
-        {            
+        {
             // Return pong response
             return Request.CreateResponse(
-                HttpStatusCode.OK, 
+                HttpStatusCode.OK,
                 new ServiceResponse()
-            {
-                Type = ResponseType.Success,
-                Message = "Pong",
-                Payload = null
-            });
+                {
+                    Type = ResponseType.Success,
+                    Message = "Pong",
+                    Payload = null
+                });
+        }
+
+        /// <summary>
+        /// Resolves a configuration value (always string).
+        /// </summary>
+        /// <returns>Specified value</returns>
+        [Route("Dock/Ping"), HttpPost, ConfigHubAuthorize]
+        public HttpResponseMessage Resolve(string path)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Publishes the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        [Route("Dock/Ping"), HttpPost, ConfigHubAuthorize]
+        public HttpResponseMessage Publish(string path, string value)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Lists the children for the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        [Route("Dock/Ping"), HttpPost, ConfigHubAuthorize]
+        public HttpResponseMessage ListChildren(string path)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Lists the configuration atoms for the specified pattern (* is wildcard).
+        /// </summary>
+        /// <param name="pattern">The pattern.</param>
+        /// <returns></returns>
+        [Route("Dock/Ping"), HttpPost, ConfigHubAuthorize]
+        public HttpResponseMessage Find(string pattern)
+        {
+            // TODO
         }
     }
 }
