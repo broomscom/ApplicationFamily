9fd0a6cfdb4284594536e875b9861795927d1aa8
Broomscom <brooks.kreutzer.analyst@outlook.com>
2016-07-01 22:10:01 -0600

    #SETUP - All day to

---
+++ CFG.Hub/Attributes/ConfigHubAuthorize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CFG.Hub.Attributes
+{
+    public class ConfigHubAuthorize
+    {
+        public ConfigHubAuthorize()
+        {
+
+        }
+    }
+}
---
+++ CFG.Hub/Controllers/DockController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CFG.Hub.Controllers
+{
+    public class DockController : ApiController
+    {
+        [Route("Dock/Ping"), HttpGet]
+        public HttpResponseMessage Ping()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new object());
+        }
+    }
+}
