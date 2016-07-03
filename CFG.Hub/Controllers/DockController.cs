93b48d74920bca2af5616da90607f1ee8456ce1f
Broomscom <brooks.kreutzer.analyst@outlook.com>
2016-07-03 00:36:53 -0600

    #IMPLPHASE - Resolve and Publish prototyped

--- CFG.Hub/Controllers/DockController.cs
+++ CFG.Hub/Controllers/DockController.cs
@@ -7,12 +7,30 @@ namespace CFG.Hub.Controllers
     using System.Web.Http;
     using Attributes;
     using Docker.Models;
-    /// <summary>
-    /// Configuration hub endpoint controller.
-    /// </summary>
-    /// <seealso cref="System.Web.Http.ApiController" />
+    using System;
+    using System.Configuration;
+    using System.Collections.Generic;
+    using System.Linq;/// <summary>
+                      /// Configuration hub endpoint controller.
+                      /// </summary>
+                      /// <seealso cref="System.Web.Http.ApiController" />
     public class DockController : ApiController
     {
+        private static bool SendClientDebugMessage = false;
+
+        public DockController()
+        {
+            // Resolve SendClientDebugMessages flag
+            try
+            {
+                SendClientDebugMessage = bool.Parse(ConfigurationManager.AppSettings["SendClientDebugMessages"]);
+            }
+            catch
+            {
+                // Don't care
+            }
+        }
+
         /// <summary>
         /// Pings this instance.
         /// </summary>
@@ -21,14 +39,14 @@ namespace CFG.Hub.Controllers
         public HttpResponseMessage Ping()
         {
             // Return pong response
-            return Request.CreateResponse(
-                HttpStatusCode.OK,
-                new ServiceResponse()
-                {
-                    Type = ResponseType.Success,
-                    Message = "Pong",
-                    Payload = null
-                });
+            try
+            {
+                return GenerateSuccessResponse("CFGDocker(Server): Pong", null);
+            }
+            catch (Exception err)
+            {
+                return GenerateExceptionResponse("CFGDocker(Server): Unexpected failure" + (SendClientDebugMessage ? " - " + err.ToString() : ""));
+            }
         }
 
         /// <summary>
@@ -38,14 +56,24 @@ namespace CFG.Hub.Controllers
         [Route("Dock/Resolve"), HttpPost, ConfigHubAuthorize]
         public HttpResponseMessage Resolve(string path)
         {
-            return Request.CreateResponse(
-                HttpStatusCode.OK,
-                new ServiceResponse()
+            try
+            {
+                // Build context
+                using (ConfigHubEFEntities context = new ConfigHubEFEntities())
                 {
-                    Type = ResponseType.Success,
-                    Message = "Pong",
-                    Payload = null
-                });
+                    // Parse the path
+                    CFGHub_SystemComponent component = null;
+                    CFGHub_ConfigAtom configAtom = ResolveAtomOrBuild(path, context, out component);
+                    context.SaveChanges();
+
+                    // Respond
+                    return GenerateSuccessResponse("CFGDocker(Server): Sending configuration atom '" + path + "'", configAtom.Value);
+                }                
+            }
+            catch (Exception err)
+            {
+                return GenerateExceptionResponse("CFGDocker(Server): Failed to find resolve path '" + path + "'" + (SendClientDebugMessage ? " - " + err.ToString() : ""));
+            }
         }
 
         /// <summary>
@@ -57,14 +85,26 @@ namespace CFG.Hub.Controllers
         [Route("Dock/Publish"), HttpPost, ConfigHubAuthorize]
         public HttpResponseMessage Publish(string path, string value)
         {
-            return Request.CreateResponse(
-                HttpStatusCode.OK,
-                new ServiceResponse()
+            try
+            {
+                // Build context
+                using (ConfigHubEFEntities context = new ConfigHubEFEntities())
                 {
-                    Type = ResponseType.Success,
-                    Message = "Pong",
-                    Payload = null
-                });
+                    // Parse the path
+                    CFGHub_SystemComponent component = null;
+                    CFGHub_ConfigAtom configAtom = ResolveAtomOrBuild(path, context, out component);
+                    configAtom.Value = value;
+                    context.CFGHub_ConfigAtom.Add(configAtom);
+                    context.SaveChanges();
+
+                    // Response
+                    return GenerateSuccessResponse("CFGDocker(Server): Publish succeeded", null);
+                }                
+            }
+            catch (Exception err)
+            {
+                return GenerateExceptionResponse("CFGDocker(Server): Failed to find publish path '" + path + "'" + (SendClientDebugMessage ? " - " + err.ToString() : ""));
+            }
         }
 
         /// <summary>
@@ -102,5 +142,87 @@ namespace CFG.Hub.Controllers
                     Payload = null
                 });
         }
+
+        #region Cores
+        private HttpResponseMessage GenerateSuccessResponse(string message, object payload)
+        {
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+                new ServiceResponse()
+                {
+                    Type = ResponseType.Success,
+                    Message = message,
+                    Payload = payload
+                });
+        }
+        private HttpResponseMessage GenerateExceptionResponse(string message)
+        {
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+                new ServiceResponse()
+                {
+                    Type = ResponseType.Error,
+                    Message = message,
+                    Payload = null
+                });
+        }
+        private CFGHub_ConfigAtom ResolveAtomOrBuild(string path, ConfigHubEFEntities context, out CFGHub_SystemComponent pathComponentResolve)
+        {
+            // Declare return atom
+            CFGHub_ConfigAtom currentAtom = null;
+
+            // Resolve component, tail and tip of path atom names
+            List<string> trailAtomPath = path.Split('.').ToList();            
+            string tipAtomName = trailAtomPath[trailAtomPath.Count - 1];
+            trailAtomPath.RemoveAt(trailAtomPath.Count - 1);
+            string componentName = trailAtomPath[0];
+            trailAtomPath.RemoveAt(0);
+
+            // Get the path component
+            pathComponentResolve = context.CFGHub_SystemComponent.Where(item => item.Name == componentName).FirstOrDefault();
+
+            // Resolve the tail atom            
+            foreach(string atomName in trailAtomPath)
+            {
+                if (currentAtom == null)
+                {
+                    currentAtom = context.CFGHub_ConfigAtom
+                        .Where(item => item.Name == componentName).FirstOrDefault();
+                }
+                else
+                {
+                    currentAtom = context.CFGHub_ConfigAtom
+                        .Where(item => item.ParentID == currentAtom.ID &&
+                        item.Name == atomName).FirstOrDefault();
+
+                    if (currentAtom == null)
+                    {
+                        // Must have the tail to store
+                        throw new Exception("Invalid path");
+                    }
+                }
+            }            
+
+            // Resolve if exists or create if does not
+            CFGHub_ConfigAtom tipAtom = context.CFGHub_ConfigAtom
+                        .Where(item => item.ParentID == currentAtom.ID &&
+                        item.Name == tipAtomName).FirstOrDefault();
+            if (tipAtom == null)
+            {
+                tipAtom = new CFGHub_ConfigAtom()
+                {
+                    ComponentID = pathComponentResolve.ID,
+                    ParentID = currentAtom.ID,
+                    ID = Guid.NewGuid(),
+                    Name = tipAtomName,                                  
+                };                              
+                return tipAtom;
+            }                
+            else
+            {
+                return tipAtom;
+            }            
+        }        
+        #endregion
     }
 }
