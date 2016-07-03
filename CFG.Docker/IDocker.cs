202907129b6738285c6d82e8df111373e002dd33
Broomscom <brooks.kreutzer.analyst@outlook.com>
2016-07-02 18:53:57 -0600

    #SETUP - Stubbed CFG docker

---
+++ CFG.Docker/IDocker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFG.Docker
+{
+    public interface IDocker
+    {
+        string Ping();
+
+        T Resolve<T>(string configurationPath);
+
+        void Publish(string configurationPath, string configurationValue);
+
+        List<string> ListChildren(string ofConfigurationPath);
+
+        List<string> Find(string configurationSearchPattern);
+    }
+}
---
+++ CFG.Docker/StandardConfigHubDocker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFG.Docker
+{
+    public class StandardConfigHubDocker : IDocker
+    {
+        public List<string> Find(string configurationSearchPattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ListChildren(string ofConfigurationPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Ping()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish(string configurationPath, string configurationValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>(string configurationPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
